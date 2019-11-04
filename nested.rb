572bbd299f9fe75ed1852edeb7ff2c4aac2ef5e1
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-04 16:54:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,5 +129,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  dennis_new_lang = "Assembly"
+  dennis_current_lang = programmer_hash[:dennis_ritchie][:known_for].to_s
+  programmer_hash[:dennis_ritchie][:known_for] = [dennis_current_lang,dennis_new_lang]
+  return programmer_hash 
 end

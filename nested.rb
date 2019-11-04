f4a925f3598fbf1675cd460fd2d4752c70fd15d4
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-04 16:48:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,11 +81,12 @@ def adding_matz
         }
     }
 
-  programmer_hash[:yukihiro_matsumoto] => 
+  temp_hash = :yukihiro_matsumoto => 
   {
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
-  }   
+  }
+  
 end
 
 def changing_alan
